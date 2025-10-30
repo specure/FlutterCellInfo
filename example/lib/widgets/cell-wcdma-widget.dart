e5e4fe1cbf2f8f7e9346ec7ed0a88feb4a0d7d46
Michal Cadrik <michal.cadrik@specure.com>
2025-10-30 12:23:46 +0100

    created ui for displaying WCDMA cell type

---
+++ example/lib/widgets/cell-wcdma-widget.dart
@@ -0,0 +1,73 @@
+
+import 'package:cell_info/models/common/cell_type.dart';
+import 'package:flutter/widgets.dart';
+
+class WcdmaMainCellInfoWidget extends StatelessWidget {
+
+  WcdmaMainCellInfoWidget({
+    Key? key,
+    this.cellInfo
+  }) : super (key: key);
+
+  final CellType? cellInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text("SubId: ${cellInfo?.wcdma?.subscriptionId}, Type: ${cellInfo?.type} * ${cellInfo?.wcdma?.type} * ${cellInfo?.wcdma?.network?.mcc}-${cellInfo?.wcdma?.network?.mnc} * ${cellInfo?.wcdma?.bandWCDMA?.name} * ${cellInfo?.wcdma?.connectionStatus}"),
+        Table (
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          columnWidths: const {
+            0: FractionColumnWidth(1/6),
+            1: FractionColumnWidth(2/6),
+            2: FractionColumnWidth(1/6),
+            3: FractionColumnWidth(2/6),
+          },
+          children: [
+            TableRow(
+                children: [
+                  Text("CI:"),
+                  Text("${cellInfo?.wcdma?.ci}"),
+                  Text("RSSI:"),
+                  Text("${cellInfo?.wcdma?.signalWCDMA?.rssi}"),
+                ]
+            ),
+            TableRow(
+                children: [
+                  Text("RNC:"),
+                  Text("${cellInfo?.wcdma?.rnc}"),
+                  Text("BER:"),
+                  Text("${cellInfo?.wcdma?.signalWCDMA?.bitErrorRate}"),
+                ]
+            ),
+            TableRow(
+                children: [
+                  Text("CID:"),
+                  Text("${cellInfo?.wcdma?.cid}"),
+                  Text("RSCP:"),
+                  Text("${cellInfo?.wcdma?.signalWCDMA?.rscp}"),
+                ]
+            ),
+            TableRow(
+                children: [
+                  Text("LAC:"),
+                  Text("${cellInfo?.wcdma?.lac}"),
+                  Text("ECIO:"),
+                  Text("${cellInfo?.wcdma?.signalWCDMA?.ecio}"),
+                ]
+            ),
+            TableRow(
+                children: [
+                  Text("PSC:"),
+                  Text("${cellInfo?.wcdma?.psc}"),
+                  Text("ECNO:"),
+                  Text("${cellInfo?.wcdma?.signalWCDMA?.ecno}"),
+                ]
+            ),
+          ],
+        )],
+    );
+  }
+}
